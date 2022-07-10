3e8e8c2aaf5d30c6bec8b63cb20adc8e3856f1b2
Vicente Daie Pinilla <vdaiep@gmail.com>
2022-07-10 15:00:26 -0400

    app ready?

--- appcursos/static/appcursos/js/scripts.js
+++ appcursos/static/appcursos/js/scripts.js
@@ -1,28 +1,67 @@
 let CURSOS_OTONO = ["MA11 - Álgebra I", "MA01 - Cálculo I", "FI01 - Física Clásica", "QC01 - Química", "BI01 - Biología"];
 let CURSOS_PRIMAVERA = ["MA12 - Álgebra II", "MA02 - Cálculo II", "EC01 - Economía", "FI01 - Física Moderna", "CC01 - Programación"];
 
-$( document ).ready(function(){
+$(document).ready(function () {
     updateCursoSelect();
     handleSemestreSelect();
+    handleSubmitButton();
+    handleExportButton();
 });
 
-function handleSemestreSelect(){
-    $("#semestre-select").on("change", function(){
+function handleSemestreSelect() {
+    $("#semestre-select").on("change", function () {
         updateCursoSelect();
     });
 };
 
-function updateCursoSelect(){
+var cached_response;
+var busy = false;
+function handleSubmitButton() {
+    $("#submit-button").on("click", function () {
+        if (!busy) {
+            busy = true;
+            anio = $("#semestre-select").val().split("-")[0];
+            semestre = $("#semestre-select").val().split("-")[1];
+            codigo = $("#curso-select").val().split(" - ")[0];
+            $.ajax({
+                url: "/",
+                type: "POST",
+                dataType: "json",
+                data: { anio: anio, semestre: semestre, codigo: codigo},
+                headers: {
+                    "X-Requested-With": "XMLHttpRequest",
+                    "X-CSRFToken": $.cookie("csrftoken"),
+                },
+                success: (response) => {
+                    cached_response = response;
+                    updateTable(response["data"]);
+                },
+                error: (error) => {
+                    console.log("Failure.");
+                }
+            });
+            busy = false;
+        };
+    });
+};
+
+function handleExportButton(){
+    $("#export-button").on("click", function(){
+        exportCSV();
+    });
+};
+
+function updateCursoSelect() {
     $("#curso-select").empty();
-    if($("#semestre-select").val().split("-")[1] == 1){
-        for(curso of CURSOS_OTONO){
+    if ($("#semestre-select").val().split("-")[1] == 1) {
+        for (curso of CURSOS_OTONO) {
             opt = document.createElement("option");
             opt.text = curso;
             opt.value = curso;
             document.getElementById("curso-select").appendChild(opt);
         };
     } else {
-        for(curso of CURSOS_PRIMAVERA){
+        for (curso of CURSOS_PRIMAVERA) {
             opt = document.createElement("option");
             opt.text = curso;
             opt.value = curso;
@@ -33,18 +72,39 @@ function updateCursoSelect(){
 
 var table;
 function updateTable(data) {
-    if(table != null){
+    if (table != null) {
         table.destroy();
     };
-    $("#latest-tweets-datatable > thead").empty();
-    $("#latest-tweets-datatable > tfoot").empty();
-    $("#latest-tweets-datatable > tbody").empty();
-    $("#latest-tweets-datatable > thead").append("<tr><th>Estudiante</th><th>Pregunta</th><th>Respuesta correcta</th><th>Respuesta estudiante</th></tr>");
-    $("#latest-tweets-datatable > tfoot").append("<tr><th>Estudiante</th><th>Pregunta</th><th>Respuesta correcta</th><th>Respuesta estudiante</th></tr>");
-    for(let i=0; i<data.length; i++){
-        $("#results-table > tbody").append(`<tr><td>${data[i]["estudiante"]}</td><td>${data[i]["pregunta"]}</td><td>${data[i]["respuesta_correcta"]}</td><td>${data[i]["respuesta_estudiante"]}</td></tr>`);
+    $("#results-table > thead").empty();
+    $("#results-table > tfoot").empty();
+    $("#results-table > tbody").empty();
+    $("#results-table > thead").append("<tr><th>Número</th><th>Enunciado</th><th>Puntaje</th><th>Respuesta correcta</th><th>Respuesta estudiante</th><th>Estudiante</th></tr>");
+    $("#results-table > tfoot").append("<tr><th>Número</th><th>Enunciado</th><th>Puntaje</th><th>Respuesta correcta</th><th>Respuesta estudiante</th><th>Estudiante</th></tr>");
+    for (let i = 0; i < data.length; i++) {
+        $("#results-table > tbody").append(`<tr><td>${data[i]["numero"]}</td><td>${data[i]["enunciado"]}</td><td>${data[i]["puntaje"]}</td><td>${data[i]["correcta"]}</td><td>${data[i]["respuesta"]}</td><td>${data[i]["nombre"]}</td></tr>`);
     };
     datatable = document.getElementById('results-table');
     table = new simpleDatatables.DataTable(datatable);
+    $("#results-label").html("Resultados: " + codigo + ", " + anio + "-" + semestre)
     $("#table-container").removeAttr("hidden");
-};  
+    $("#export-button-container").removeAttr("hidden");
+};
+
+function exportCSV(){
+    rows = [["Pregunta", "Enunciado", "Puntaje", "Respuesta_correcta", "Respuesta_estudiante", "Nombre_estudiante"]];
+    for(dict of cached_response["data"]){
+        rows.push([dict["numero"], dict["enunciado"], dict["puntaje"], dict["correcta"], dict["respuesta"], dict["nombre"]]);
+    };
+    let csvContent = "data:text/csv;charset=utf-8,";
+    rows.forEach(function(rowArray) {
+        let row = rowArray.join(",");
+        csvContent += row + "\r\n";
+    });
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    filename = cached_response["input"]["codigo"] + "_" + cached_response["input"]["anio"] + "_" + cached_response["input"]["semestre"] + ".csv";
+    link.setAttribute("download", filename);
+    document.body.appendChild(link);
+    link.click();
+};
--- appcursos/views.py
+++ appcursos/views.py
@@ -1,18 +1,36 @@
-from django.http import HttpResponse
+import json
+from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
 import logging
 logger = logging.getLogger("mylogger")
 
-from .models import Curso, Estudiante
+from .models import Curso, Estudiante, Pregunta, Respuesta
 
 
 @require_http_methods(["GET", "POST"])
 def index(request):
     if request.method == "POST":
-        anio = str(request.POST["semestre"].split("-")[0])
-        semestre = str(request.POST["semestre"].split("-")[1])
-        codigo = request.POST["curso"].split(" - ")[0]
-        id_curso = Curso.objects.filter(anio=anio).filter(semestre=semestre).filter(codigo=codigo)[0].id
-        #return HttpResponseRedirect('/thanks/')
+        anio = request.POST["anio"]
+        semestre = request.POST["semestre"]
+        codigo = request.POST["codigo"]
+        id_curso = Curso.objects.filter(anio=anio, semestre=semestre, codigo=codigo)[0].id
+        preguntas = Pregunta.objects.filter(curso=id_curso)
+        out = []
+        n = 1
+        for p in preguntas:
+            id_pregunta = p.id
+            respuestas_p = Respuesta.objects.filter(pregunta=id_pregunta)
+            for r in respuestas_p:
+                line = {
+                    "numero": n,
+                    "enunciado": p.enunciado,
+                    "puntaje": p.puntaje,
+                    "correcta": p.correcta,
+                    "respuesta": r.respuesta,
+                    "nombre": Estudiante.objects.get(id=r.estudiante.id).nombre
+                }
+                out.append(line)
+            n = n + 1
+        return JsonResponse({"data": out, "input": {"anio": anio, "semestre": semestre, "codigo": codigo}})
     return render(request, "appcursos/index.html")
