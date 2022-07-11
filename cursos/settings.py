8b782987522d5160e7ec81b655388a1813ec9eb8
Vicente Daie Pinilla <vdaiep@gmail.com>
2022-07-10 22:09:22 -0400

    final commit

--- cursos/settings.py
+++ cursos/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', 'http://appcursos-dev2.us-east-1.elasticbeanstalk.com/' 'https://appcursos-dev2.us-east-1.elasticbeanstalk.com/', 'http://play2.vdaiep.cl/', 'https://play2.vdaiep.cl/']
 
 
 # Application definition
